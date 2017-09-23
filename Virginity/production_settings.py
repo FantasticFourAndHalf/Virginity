59f4ae470ef022cd1ec0e6a534ad84db35745221
zhufyakvv <zhufyakvv@gmail.com>
2017-09-23 13:26:54 +0300

    Image Backend :sunrise:
    - Image now Stores at ImGur. Fixes image missing in production.
    - Production Settings

--- Virginity/production_settings.py
+++ Virginity/production_settings.py
@@ -7,6 +7,7 @@ from Virginity.settings import *
 
 DEBUG = False
 
+
 # ImGur Client Production
 IMGUR_CLIENT_ID = '2bcda18a072f658'
 IMGUR_CLIENT_SECRET = 'eb16a3a1783ac786362796970602dbf9e59d6768'
