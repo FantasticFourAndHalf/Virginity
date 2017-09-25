638776e1b474546d79aeb0b149f9c6fd74f31691
zhufyakvv <zhufyakvv@gmail.com>
2017-09-25 10:17:35 +0300

    Admin Panel :new_moon_with_face:
    - Admin Panel now Material based.
    - Models like Dishes and Order have sub models on edit/creation page.
    - Sub Models Replaced.

--- Virginity/production_settings.py
+++ Virginity/production_settings.py
@@ -10,5 +10,7 @@ from Virginity.settings import *
 
 ALLOWED_HOSTS += ['.herokuapp.com']
 
+SITE_ID = 2
+
 IMGUR_CLIENT_ID = '2bcda18a072f658'
 IMGUR_CLIENT_SECRET = 'eb16a3a1783ac786362796970602dbf9e59d6768'
