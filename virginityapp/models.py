1d146a80f49f3619ed5d746b1dc71ffd5fe1756d
zhufyakvv <zhufyakvv@gmail.com>
2017-10-03 12:10:36 +0300

    Color Scheme :coffee:
    - Changed experimental color scheme.

--- virginityapp/models.py
+++ virginityapp/models.py
@@ -15,7 +15,7 @@ class User(AbstractUser):
     address = models.CharField(max_length=256, blank=True)
 
     def __str__(self):
-        return str(self.id) + " " + self.first_name + " " + self.last_name
+        return str(self.id) + " " + str(self.username) + "  " + self.first_name + " " + self.last_name
 
 
 class Address(models.Model):
